8f1e7405fbba63b70f7dacb165d4d5d621e569ea
Rodman <rodman.iniguez@epn.edu.ec>
2021-07-14 20:23:19 -0500

    SharedPreferences

--- app/src/main/java/com/iniguezrodman/cazarpatos/Constants.kt
+++ app/src/main/java/com/iniguezrodman/cazarpatos/Constants.kt
@@ -1,3 +1,7 @@
 package com.iniguezrodman.cazarpatos
 
 const val EXTRA_LOGIN = "EXTRA_LOGIN"
+const val LOGIN_KEY = "LOGIN_KEY"
+const val PASSWORD_KEY = "PASSWORD_KEY"
+const val MIN_PASSWORD_LENGTH = 8
+const val SHAREDINFO_FILENAME = "mySharedInformation.dat"
--- app/src/main/java/com/iniguezrodman/cazarpatos/LoginActivity.kt
+++ app/src/main/java/com/iniguezrodman/cazarpatos/LoginActivity.kt
@@ -1,10 +1,12 @@
 package com.iniguezrodman.cazarpatos
 
+import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
@@ -13,14 +15,21 @@ class LoginActivity : AppCompatActivity() {
     lateinit var buttonLogin: Button
     lateinit var buttonNewUser:Button
     lateinit var mediaPlayer: MediaPlayer
+    lateinit var checkBoxRecordarme: CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
         //Inicialización de variables
         editTextEmail = findViewById(R.id.editTextEmail)
         editTextPassword = findViewById(R.id.editTextPassword)
         buttonLogin = findViewById(R.id.buttonLogin)
         buttonNewUser = findViewById(R.id.buttonNewUser)
+        checkBoxRecordarme = findViewById(R.id.checkBoxRecordarme)
+        editTextEmail.setText ( sharedPref.getString(LOGIN_KEY,"") )
+        editTextPassword.setText ( sharedPref.getString(PASSWORD_KEY,"") )
+
         //Eventos clic
         buttonLogin.setOnClickListener {
             val email = editTextEmail.text.toString()
@@ -29,6 +38,18 @@ class LoginActivity : AppCompatActivity() {
             if(!ValidarDatosRequeridos())
                 return@setOnClickListener
             //Si pasa validación de datos requeridos, ir a pantalla principal
+            if(checkBoxRecordarme.isChecked){
+                val editor = sharedPref.edit()
+                editor.putString(LOGIN_KEY,editTextEmail.text.toString())
+                editor.putString(PASSWORD_KEY,editTextPassword.text.toString())
+                editor.commit()
+            }
+            else{
+                val editor = sharedPref.edit()
+                editor.putString(LOGIN_KEY,"")
+                editor.putString(PASSWORD_KEY,"")
+                editor.commit()
+            }
             val intencion = Intent(this, MainActivity::class.java)
             intencion.putExtra(EXTRA_LOGIN, email)
             startActivity(intencion)
@@ -39,6 +60,12 @@ class LoginActivity : AppCompatActivity() {
         mediaPlayer=MediaPlayer.create(this, R.raw.title_screen)
         mediaPlayer.start()
     }
+    private fun ActivarRecuerdame():Boolean{
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+        if(sharedPref.getString(LOGIN_KEY,"") != null&& sharedPref.getString(PASSWORD_KEY,"")!=null) {
+            return true
+        }
+        return false}
 
     private fun ValidarDatosRequeridos():Boolean{
         val email = editTextEmail.text.toString()
